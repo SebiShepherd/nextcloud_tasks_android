e73808f641e36f80ec249b7bff1af4d3575e843b
Sebastian Schäfer <trueinnov@gmail.com>
2025-12-20 22:54:55 +0100

    Fix ktlint import ordering in TasksRepository

--- domain/src/main/kotlin/com/nextcloud/tasks/domain/repository/TasksRepository.kt
+++ domain/src/main/kotlin/com/nextcloud/tasks/domain/repository/TasksRepository.kt
@@ -1,9 +1,9 @@
 package com.nextcloud.tasks.domain.repository
 
+import com.nextcloud.tasks.domain.model.Tag
 import com.nextcloud.tasks.domain.model.Task
 import com.nextcloud.tasks.domain.model.TaskDraft
 import com.nextcloud.tasks.domain.model.TaskList
-import com.nextcloud.tasks.domain.model.Tag
 import kotlinx.coroutines.flow.Flow
 
 interface TasksRepository {
