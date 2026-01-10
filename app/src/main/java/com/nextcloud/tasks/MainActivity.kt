f63923ef8ccfa4849c680d609b15f7435be78159
Sebastian Schäfer <trueinnov@gmail.com>
2026-01-10 22:02:44 +0100

    Fix import order in MainActivity.kt

--- app/src/main/java/com/nextcloud/tasks/MainActivity.kt
+++ app/src/main/java/com/nextcloud/tasks/MainActivity.kt
@@ -2,8 +2,8 @@ package com.nextcloud.tasks
 
 import android.content.res.Configuration
 import android.os.Bundle
-import androidx.activity.compose.setContent
 import androidx.activity.compose.BackHandler
+import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
